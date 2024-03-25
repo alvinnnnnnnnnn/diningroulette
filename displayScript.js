d6ba7253b78fd5d63cec41fa4d594649a8fb1f1a
alvinwongster <alvinwongster@gmail.com>
2024-03-25 16:57:35 +0800

    added go back button

--- displayScript.js
+++ displayScript.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
     const name = params.get('name');
-    const type = params.get('type');
-    const cuisines = params.get('cuisines');
     const location = params.get('location');
     
     document.getElementById('placeInfo').innerHTML = `
         <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Type:</strong> ${type}</p>
-        <p><strong>Cuisines:</strong> ${cuisines}</p>
         <p><strong>Location:</strong> ${location}</p>
     `;
+
+    document.getElementById('goBackButton').addEventListener('click', function() {
+        window.location.href = `index.html`;
+    });
 });
